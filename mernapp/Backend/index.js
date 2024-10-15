61a7fda48ce14baecbdb81c31e9204fc789905c3
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-10-15 21:55:41 +0530

    Update index.js

--- mernapp/Backend/index.js
+++ mernapp/Backend/index.js
@@ -3,20 +3,12 @@ const app = express()
 const port = 5000
 const mongoDB = require("./db")
 
-app.use((req, res, next)=> {
-  res.setheader("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers",
-     "Origin, X-Requested-With, Content-Type, Accept"
-    );
-  next();
-});
-
 
 app.use(express.json())
-app.use('/api',require("./routes/CreatUser"));
+app.use('/api', require("./Routes/CreatUser"));
 app.get('/', (req, res) => {
   res.send('Hello World!')
-}) 
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
