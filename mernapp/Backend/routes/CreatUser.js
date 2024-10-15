bcbc8f757e49b8635e2d3e86461e87298319dd42
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-10-15 21:55:45 +0530

    Update CreatUser.js

--- mernapp/Backend/routes/CreatUser.js
+++ mernapp/Backend/routes/CreatUser.js
@@ -1,31 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
-const { body, validationResult } = require('express-validator');
-router.post("/creatuser", [
-    body('email').isEmail(),
-    body('name').isLength({ min: 10 }),
-    // password must be at least 5 chars long
-    body('password', 'Incorrect Password').isLength({ min: 5 })]
-    , async (req, res) => {
 
-        // Finds the validation errors in this request and wraps them in an object with handy functions
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
+router.post("/creatuser", async (req, res) => {
+    try {
+        await User.create({
+            name: req.body.name,
+            password:req.body.password,
+            email: req.body.email,
+            location: req.body.location
+        })
+        res.json({ success: true });
 
-        try {
-            await User.create({
-                name: req.body.name,
-                password: req.body.password,
-                email: req.body.email,
-                location: req.body.location
-            }).then(res.json({ success: true }));
-
-        } catch (error) {
-            console.log(error)
-            res.json({ success: false });
-        }
-    })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false });
+    }
+})
 module.exports = router;
