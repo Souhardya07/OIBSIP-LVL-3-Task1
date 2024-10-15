a3998bddb4a47f8b1b0d033b161070b318a7d1ea
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-10-15 13:28:11 +0530

    Update App.js

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -10,6 +10,7 @@ import Login from './screen/Login';
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import Signup from './screen/Signup.js';
 function App() {
   return (
     <Router>
@@ -17,7 +18,7 @@ function App() {
       <Routes>
            <Route exact path="/" element={<Home/>}/>
            <Route exact path="/Login" element={<Login/>}/>
-
+           <Route exact path="/creatuser" element={<Signup/>}/>
       </Routes>
     </div>
     </Router>
