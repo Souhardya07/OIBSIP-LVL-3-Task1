5d003456048f8d477abbc47a679bf74fbb1fa975
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-10-15 13:27:14 +0530

    Update db.js

--- mernapp/Backend/db.js
+++ mernapp/Backend/db.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://FOOGLE:07032003#Sc@cluster0.srjew.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const mongoDB = async () => {
-    await mongoose.connect(mongoURI, { useNewUrlParser: true }, ()=>{
-
-            console.log("connected");
-    });
+const mongoURI = "mongodb+srv://FOOGLE:07032003f@cluster0.srjew.mongodb.net/FOOGLEmern?retryWrites=true&w=majority&appName=Cluster0"
+const mongoDB =async() =>{
+  await mongoose.connect(mongoURI, {useNewUrlParser: true ,useUnifiedTopology: true},async(err,result) => {
+   if (err) console.log("---",err)
+    else{
+    console.log("connected");
+     const fectched_data = await mongoose.connection.db.collection("sample")
+     fectched_data.find({}).toArray( function(err,data){
+      if(err) console.log(err);
+      else console.log();
+     })
+    }
+  });
 
 }
 
